516b4fb8dcadf91b8a8d3830226ee64e2b4467d4
gmogro <guillermomogro51@gmail.com>
2022-06-28 11:46:00 -0300

    pendientes actualizar - eliminar

--- src/Entidades/Cliente.java
+++ src/Entidades/Cliente.java
@@ -1,5 +1,216 @@
 package Entidades;
 
-public class Cliente {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
+public class Cliente {
+	private String cuil;
+	private String resposabilidadAfip;
+
+	public Cliente() {
+	}
+	public Cliente(String cuil, String resposabilidadAfip) {
+		super();
+		this.cuil = cuil;
+		this.resposabilidadAfip = resposabilidadAfip;
+	}
+	
+	public String getCuil() {
+		return cuil;
+	}
+	public void setCuil(String cuil) {
+		this.cuil = cuil;
+	}
+	public String getResposabilidadAfip() {
+		return resposabilidadAfip;
+	}
+	public void setResposabilidadAfip(String resposabilidadAfip) {
+		this.resposabilidadAfip = resposabilidadAfip;
+	}
+	
+	//CRUD
+	
+	public void crearCliente(Connection conexion) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("#######################");
+		System.out.println("Datos del Cliente");
+		System.out.println("#######################");
+		System.out.println("Apellido :");
+		String apellido = sc.nextLine(); 
+		System.out.println("Nombre :");
+		String nombre = sc.nextLine();
+		System.out.println("Domicilio :");
+		String domicilio = sc.nextLine();
+		System.out.println("Documento :");
+		String documento = sc.nextLine();
+		System.out.println("Provincia :");
+		String provincia = sc.nextLine();
+		System.out.println("Codigo Postal :");
+		String codigoPostal = sc.nextLine();
+		System.out.println("Fecha de Nacimiento : ");
+		String fechaNacimiento = sc.nextLine();
+		System.out.println("Telefono : ");
+		String telefono = sc.nextLine();
+		System.out.println("Cuil : ");
+		String cuil = sc.nextLine();
+		System.out.println("Responsabilidad en Afip : ");
+		String afip = sc.nextLine();
+		
+		Statement statement = null;
+		String sql;
+		ResultSet rs;
+		PreparedStatement stmt;
+		
+		try {
+			statement = conexion.createStatement();
+			sql = "SELECT idpersona FROM persona order by idpersona DESC LIMIT 1;";
+			rs = statement.executeQuery(sql);
+			int idpersona = 0;
+			while(rs.next()) 
+			{
+				idpersona = rs.getInt("idpersona");
+			}
+			
+			stmt = conexion.prepareStatement("INSERT INTO persona VALUES (?,?,?,?,?,?,?,?,?)");
+        	stmt.setInt(1,idpersona+1);
+        	stmt.setString(2,nombre);
+        	stmt.setString(3,apellido);
+        	stmt.setString(4,domicilio);
+        	stmt.setString(5,documento);
+        	stmt.setString(6,provincia);
+        	stmt.setString(7,codigoPostal);
+        	stmt.setString(8,fechaNacimiento);
+        	stmt.setString(9,telefono);
+        	
+        	int response = stmt.executeUpdate();
+        	if(response>0) 
+        	{
+        		System.out.println("se inserto persona correctamente");
+        	}
+        	
+        	sql = "SELECT idcliente FROM cliente order by idcliente DESC LIMIT 1;";
+			rs = statement.executeQuery(sql);
+			int idcliente = 0;
+			while(rs.next()) 
+			{
+				idcliente = rs.getInt("idcliente");
+			}
+        	
+			stmt = conexion.prepareStatement("INSERT INTO cliente VALUES (?,?,?,?)");
+        	stmt.setInt(1,idcliente+1);
+        	stmt.setInt(2,idpersona+1);
+        	stmt.setString(3,cuil);
+        	stmt.setString(4,afip);
+        	
+        	response = stmt.executeUpdate();
+        	if(response>0) 
+        	{
+        		System.out.println("se inserto cliente correctamente");
+        	}
+		} catch (SQLException sqle){
+            System.out.println("SQLState: "+ sqle.getSQLState());
+            System.out.println("SQLErrorCode: " + sqle.getErrorCode());
+            sqle.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+	}
+	
+	public void actualizarCliente(Connection conexion) 
+	{
+		Scanner sc = new Scanner(System.in);
+		Statement statement = null;
+		String sql;
+		ResultSet rs;
+		PreparedStatement stmt;
+		
+		try {
+			statement = conexion.createStatement();
+			sql = "SELECT idcliente,Nombre,Apellido"
+					+ "Domicilio FROM persona order by cliente DESC LIMIT 1;";
+			rs = statement.executeQuery(sql);
+			System.out.println("Seleccione Cliente");
+			while(rs.next()) 
+			{
+				int idcliente = rs.getInt("idcliente");
+				String nombre = rs.getString("Nombre");
+				System.out.println(idcliente + " - " + nombre);
+			}
+			System.out.println("Cancelar seleccione 0");
+			int idcliente  = sc.nextInt();
+			if(idcliente!=0) {
+				System.out.println("#######################");
+				System.out.println("Datos del Cliente");
+				System.out.println("#######################");
+				System.out.println("Apellido :");
+				String apellido = sc.nextLine(); 
+				System.out.println("Nombre :");
+				String nombre = sc.nextLine();
+				System.out.println("Domicilio :");
+				String domicilio = sc.nextLine();
+				System.out.println("Documento :");
+				String documento = sc.nextLine();
+				System.out.println("Provincia :");
+				String provincia = sc.nextLine();
+				System.out.println("Codigo Postal :");
+				String codigoPostal = sc.nextLine();
+				System.out.println("Fecha de Nacimiento : ");
+				String fechaNacimiento = sc.nextLine();
+				System.out.println("Telefono : ");
+				String telefono = sc.nextLine();
+				System.out.println("Cuil : ");
+				String cuil = sc.nextLine();
+				System.out.println("Responsabilidad en Afip : ");
+				String afip = sc.nextLine();
+				stmt = conexion.prepareStatement("UPDATE INTO persona VALUES (?,?,?,?,?,?,?,?,?)");
+	        	stmt.setInt(1,idpersona+1);
+	        	stmt.setString(2,nombre);
+	        	stmt.setString(3,apellido);
+	        	stmt.setString(4,domicilio);
+	        	stmt.setString(5,documento);
+	        	stmt.setString(6,provincia);
+	        	stmt.setString(7,codigoPostal);
+	        	stmt.setString(8,fechaNacimiento);
+	        	stmt.setString(9,telefono);
+	        	
+	        	int response = stmt.executeUpdate();
+	        	if(response>0) 
+	        	{
+	        		System.out.println("se inserto persona correctamente");
+	        	}
+				stmt = conexion.prepareStatement("UPDATE INTO cliente VALUES (?,?,?,?)");
+	        	stmt.setInt(1,idcliente+1);
+	        	stmt.setInt(2,idpersona+1);
+	        	stmt.setString(3,cuil);
+	        	stmt.setString(4,afip);
+	        	
+	        	response = stmt.executeUpdate();
+	        	if(response>0) 
+	        	{
+	        		System.out.println("se inserto cliente correctamente");
+	        	}
+			} catch (SQLException sqle){
+	            System.out.println("SQLState: "+ sqle.getSQLState());
+	            System.out.println("SQLErrorCode: " + sqle.getErrorCode());
+	            sqle.printStackTrace();
+	        }catch (Exception e){
+	            e.printStackTrace();
+	        }
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
--- src/Entidades/Persona.java
+++ src/Entidades/Persona.java
@@ -1,5 +1,74 @@
 package Entidades;
 
 public class Persona {
+	private String nombre;
+	private String apellido;
+	private String domicilio;
+	private String documento;
+	private String provincia;
+	private String codigoPostal;
+	private String fechaNacimiento;
+	private String telefono;
 	
+	public Persona() {
+	}
+	public Persona(String nombre, String apellido, String domicilio, String documento, String provincia,
+			String codigoPostal, String fechaNacimiento, String telefono) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.domicilio = domicilio;
+		this.documento = documento;
+		this.provincia = provincia;
+		this.codigoPostal = codigoPostal;
+		this.fechaNacimiento = fechaNacimiento;
+		this.telefono = telefono;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDomicilio() {
+		return domicilio;
+	}
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+	public String getDocumento() {
+		return documento;
+	}
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
 }
